1d81fa42ee83565966abde8a22358b2f7e17d187
Olga <olgaalaquas@gmail.com>
2024-07-10 10:58:38 +0200

    created a dataserver

--- main.js
+++ main.js
@@ -1,23 +1,31 @@
-'use strict';
+"use strict";
 const formSearch = document.querySelector(".js-form-search");
 const formList = document.querySelector(".js-list-tasks");
 const buttonAgregar = document.querySelector(".js-button1");
 const buttonBuscar = document.querySelector(".js-button2");
 const listTasks = document.querySelector(".js-list");
 
-const tasks = [
-    { name: "Aprender flexbox", completed: true, id: 1},
-    { name: "Maquetar la página web del proyecto", completed: true, id:2},
-    { name: "Estudiar la lección invertida", completed: true, id:3},
-    { name: "Aprender cómo se realizan las peticiones al servidor en JavaScript", completed: false, id:4},
-    ];
+const GITHUB_USER = "olgabonillo";
+const SERVER_URL = `https://dev.adalab.es/api/todo/${GITHUB_USER}`;
 
-    let taskHTML = '';
+function renderTasks (tasks) {
+    let taskHTML = "";
     tasks.forEach((task) => {
-        const classli = task.completed === true ? 'tachado' : ''
-        const ischecks = task.completed === true ? 'checked' : ''
-        taskHTML += `<li class='${classli}'> <input type='checkbox' id='${task.id}'${ischecks}>${task.name}</li>`;
-        
+      const classli = task.completed === true ? "tachado" : "";
+      const ischecks = task.completed === true ? "checked" : "";
+      taskHTML += `<li class='${classli}'> <input type='checkbox' id='${task.id}'${ischecks}>${task.name}</li>`;
     });
     listTasks.innerHTML = taskHTML;
+}
 
+let task = [];
+fetch(SERVER_URL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then((data) => {
+    const tasks = data.results;
+    console.log(data);
+
+  renderTasks(tasks);
+  });
