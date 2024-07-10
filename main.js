ccd211fa3ae1f674f6b865bb5e1bbb2e6f5d1911
Olga <olgaalaquas@gmail.com>
2024-07-10 11:13:41 +0200

    changes in ejercice

--- main.js
+++ main.js
@@ -4,17 +4,22 @@ const formList = document.querySelector(".js-list-tasks");
 const buttonAgregar = document.querySelector(".js-button1");
 const buttonBuscar = document.querySelector(".js-button2");
 const listTasks = document.querySelector(".js-list");
+const tareasTotales = document.querySelector(".js-frase");
 
 const GITHUB_USER = "olgabonillo";
 const SERVER_URL = `https://dev.adalab.es/api/todo/${GITHUB_USER}`;
 
 function renderTasks (tasks) {
     let taskHTML = "";
+    let totalTask = "";
+
     tasks.forEach((task) => {
       const classli = task.completed === true ? "tachado" : "";
       const ischecks = task.completed === true ? "checked" : "";
+      totalTask += `<p>Tienes X tareas</p> <input>`
       taskHTML += `<li class='${classli}'> <input type='checkbox' id='${task.id}'${ischecks}>${task.name}</li>`;
     });
+    tareasTotales.innerHTML = totalTask;
     listTasks.innerHTML = taskHTML;
 }
 
