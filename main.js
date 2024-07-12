f1988874d6fc9c91c1ccb35dffdd10ab720a4599
griseldagomez <grinoe123@gmail.com>
2024-07-12 11:26:49 +0200

    changes

--- main.js
+++ main.js
@@ -9,24 +9,32 @@ const tareasTotales = document.querySelector(".js-frase");
 const GITHUB_USER = "olgabonillo";
 const SERVER_URL = `https://dev.adalab.es/api/todo/${GITHUB_USER}`;
 
-function renderTasks (tasks) {
-    let taskHTML = "";
 
-    tasks.forEach((task) => {
-      const classli = task.completed === true ? "tachado" : "";
-      const ischecks = task.completed === true ? "checked" : "";
-      taskHTML += `<li class='${classli}'> <input type='checkbox' id='${task.id}'${ischecks}>${task.name}</li>`;
-    });
-    //se guarda en una variable la cantidad de tareas que estan hechas y las que faltan por hacer para
-    //se crea un filter y como condicion se usa task completed y no completed y luego obtengo el length.
-    const completedTasks = tasks.filter((task) => task.completed).length;
-    const todoTasks = tasks.length - completedTasks;
+//EJERCICIO DOM 
+function renderTasks(tasks) {
+  let taskHTML = "";
 
-    tareasTotales.innerHTML = `Tienes ${tasks.length} tareas. ${completedTasks} completadas y ${todoTasks} por realizar.`;
-    listTasks.innerHTML = taskHTML;
+  tasks.forEach((task) => {
+    const classli = task.completed === true ? "tachado" : "";
+    const ischecks = task.completed === true ? "checked" : "";
+    taskHTML += `<li class='${classli}'> <input type='checkbox' id='${task.id}'${ischecks}>${task.name}</li>`;
+  });
+
+
+
+
+  
+  //EJERCICIO FILTER 
+  //se guarda en una variable la cantidad de tareas que estan hechas y las que faltan por hacer para
+  //se crea un filter y como condicion se usa task completed y no completed y luego obtengo el length.
+  const completedTasks = tasks.filter((task) => task.completed).length;
+  const todoTasks = tasks.length - completedTasks;
+
+  tareasTotales.innerHTML = `Tienes ${tasks.length} tareas. ${completedTasks} completadas y ${todoTasks} por realizar.`;
+  listTasks.innerHTML = taskHTML;
 
 }
-
+//EJERCICIO FECHT
 let task = [];
 fetch(SERVER_URL)
   .then(function (response) {
@@ -36,5 +44,5 @@ fetch(SERVER_URL)
     const tasks = data.results;
     console.log(data);
 
-  renderTasks(tasks);
+    renderTasks(tasks);
   });
